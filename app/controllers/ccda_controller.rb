18b32b5edc8ea61f3530a436e1c156b55c163d0d
Andy Gregorowicz <andy@mitre.org>
2015-03-05 13:21:18 -0500

    Syntax fixes, pulling the document from the request and setting it up

--- app/controllers/ccda_controller.rb
+++ app/controllers/ccda_controller.rb
@@ -3,12 +3,14 @@ require 'json'
 class CcdaController < ApplicationController
 
   def create
-    doc = Nokogiri::XML(xml_data)
+    doc = Nokogiri::XML(request.body)
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
     patient_data = HealthDataStandards::Import::CCDA::PatientImporter.instance.parse_ccda(doc)
     record = HealthDataStandards::Record.update_or_create(patient_data)
-    File.open('patient_json/' + record.medical_record_number + '.json') do |f| {
+    File.open('patient_json/' + record.medical_record_number + '.json') do |f|
       f.write(record.to_json)
-    }
+    end
   end
 
 end
