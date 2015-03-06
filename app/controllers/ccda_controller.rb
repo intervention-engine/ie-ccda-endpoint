cfe33bb20709781b5ede2dd71e16d0f1c3997b19
Andy Gregorowicz <andy@mitre.org>
2015-03-06 16:15:52 -0500

    Now just writing the JSON back as the response
    Eliminated the call to update_or_create since we already get a Record
    object back

--- app/controllers/ccda_controller.rb
+++ app/controllers/ccda_controller.rb
@@ -7,10 +7,7 @@ class CcdaController < ApplicationController
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
     patient_data = HealthDataStandards::Import::CCDA::PatientImporter.instance.parse_ccda(doc)
-    record = HealthDataStandards::Record.update_or_create(patient_data)
-    File.open('patient_json/' + record.medical_record_number + '.json') do |f|
-      f.write(record.to_json)
-    end
+    render json: patient_data.to_json
   end
 
 end
